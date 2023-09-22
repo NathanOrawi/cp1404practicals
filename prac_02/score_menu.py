8899c712b1e59e640c3db404947decd5f49ee902
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-09-23 04:05:42 +1000

    Refactor score_menu program adding function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,32 +1,54 @@
-from scores import display_score
-from password_start import hide_character
+"""Score menu"""
 
-MENU = """G)et a valid score
+MENU = """(G)et a valid score
 (P)rint result
 (S)how stars
 (Q)uit"""
 
 
 def main():
+    """main function"""
     print(MENU)
-    choice = input("Choice: ")
+    choice = input("Choice: ").upper()
     while choice != 'Q':
-        score = float(input("Enter score: "))
         if choice == 'G':
+            score = float(input("Enter score: "))
             while 0 > score > 100:
                 print("Invalid score input")
                 score = float(input("Enter score: "))
+            print("score is valid")
         elif choice == 'P':
+            score = float(input("Enter score: "))
             result = display_score(score)
             print(result)
         elif choice == 'S':
-            as_many_stars = hide_character(score)
+            score = int(input("Enter score: "))
+            as_many_stars = show_star(score)
             print(as_many_stars)
         else:
             print("Invalid error message")
         print(MENU)
-        choice = input("Choice: ")
+        choice = input("Choice: ").upper()
     print("Taa!")
+
+
+def display_score(score):
+    """displays the result of score"""
+    if 100 < score > 0:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
+
+
+def show_star(length):
+    """displays equivalent star of score """
+    amount = '*' * length
+    return amount
 
 
 main()
