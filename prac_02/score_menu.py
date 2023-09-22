9fd78c59a0fed348f5fc8b57771322bba7d35387
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-09-23 02:59:52 +1000

    Refactor score_menu program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,15 +1,32 @@
-display menu
-get choice
-while choice != <quit option>
-    if choice == <first option>
-        <do first task>
-    else if choice == <second option>
-        <do second task>
-    ...
-    else if choice == <n-th option>
-        <do n-th task>
-    else
-        display invalid input error message
-    display menu
-    get choice
-<do final thing, if needed>
+from scores import display_score
+from password_start import hide_character
+
+MENU = """G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    print(MENU)
+    choice = input("Choice: ")
+    while choice != 'Q':
+        score = float(input("Enter score: "))
+        if choice == 'G':
+            while 0 > score > 100:
+                print("Invalid score input")
+                score = float(input("Enter score: "))
+        elif choice == 'P':
+            result = display_score(score)
+            print(result)
+        elif choice == 'S':
+            as_many_stars = hide_character(score)
+            print(as_many_stars)
+        else:
+            print("Invalid error message")
+        print(MENU)
+        choice = input("Choice: ")
+    print("Taa!")
+
+
+main()
