29fdc1bbeb09988b4ce5afed44dff25105b18613
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-04 15:50:21 +1000

    Refacto exception_demo.py program

--- prac_03/exception_demo.py
+++ prac_03/exception_demo.py
@@ -5,21 +5,23 @@ Answer the following questions:
 2. When will a ZeroDivisionError occur?
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
+#
+# try:
+#     numerator = int(input("Enter the numerator: "))
+#     denominator = int(input("Enter the denominator: "))
+#     fraction = numerator / denominator
+#     print(fraction)
+# except ValueError:
+#     print("Numerator and denominator must be valid numbers!")
+# except ZeroDivisionError:
+#     print("Cannot divide by zero!")
+# print("Finished.")
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-print("Finished.")
+"""When will a ValueError occur?
+        when numerator and denominator input is not a integer
+    When will a ZeroDivisionError occur?
+        when denominator input is 0
+    Could you change the code to avoid the possibility of a ZeroDivisionError?
+        denominator = int(input("Enter the denominator (wont except zero): "))
+"""
 
-# When will a ValueError occur?
-    # when numerator and denominator input is not a integer
-# When will a ZeroDivisionError occur?
-    # when denominator input is 0
-# Could you change the code to avoid the possibility of a ZeroDivisionError?
-    #  denominator = int(input("Enter the denominator (wont except zero): "))
