f1cafdffadd5a237f023cca0e9c656d263c2ccff
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-14 06:21:57 +1000

    Refactor by documenting code

--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -1,3 +1,9 @@
+"""
+CP1404/CP5632 Practical
+Intermediat Excercises
+"""
+
+"""Basic list operations"""
 number_of_entries = 3
 numbers = []
 for number_of_entry in range(1, number_of_entries + 1):
@@ -16,6 +22,7 @@ print(f"The smallest number is {smallest_number}")
 print(f"The largest number is {largest_number}")
 print(f"The average of the numbers is {average_of_numbers}")
 
+"""Woefully inadequate security checker"""
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username = input("User name: ")
