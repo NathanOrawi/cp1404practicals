c8ae8c276341a9590a990b0fcee1c80075064c00
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-16 07:19:27 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,32 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+
+def main():
+    """Asks the user for their 'short' state and prints the full state"""
+    display_dictionary(CODE_TO_NAME)
+    state_code = (input("Enter short state: ")).upper()
+    display_value(state_code)
+    """Write manu to select which function to call"""
+
+
+def display_dictionary(dictionary):
+    """Display the dictionary one item a line"""
+    for state_code in dictionary:
+        print(f"{state_code:3} is {dictionary[state_code]:3}")
+
+
+def display_value(key):
+    """Display the value of a dictionary given a key"""
+    while key != "":
+        try:
+            print(key, "is", CODE_TO_NAME[key])
+        except KeyError:
+            print("Invalid short state")
+        break
+
+
+main()
