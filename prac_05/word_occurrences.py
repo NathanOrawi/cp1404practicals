9f24d5c5b0284b850cec66d2c89df5a4abcfe0fc
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-10-16 12:36:23 +1000

    Functionalize code. count_word_occurrences(text)

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,13 +6,27 @@ Estimate: 20 minutes
 Actual: 41 minutes
 """
 
-string = input("Enter phrase: ")
-words = sorted(string.split())
 
-word_to_count = {}
-for word in words:
-    word_to_count[word] = word_to_count.get(word, 0) + 1
+def main():
+    """Asks user for text to count word occurrences"""
+    text = input("Enter phrase: ")
+    count_word_occurrences(text)
 
-max_length = max(len(name) for name in list(word_to_count.keys()))
-for word, count in word_to_count.items():
-    print(f"{word:{max_length}} : {count}")
+
+def count_word_occurrences(text):
+    """Count the occurrences of words in a text,
+    stores as dictionary, sorts it and displays"""
+    try:
+        words = sorted(text.split())
+        word_to_count = {}
+        for word in words:
+            word_to_count[word] = word_to_count.get(word, 0) + 1
+        max_length = max(len(name) for name in list(word_to_count.keys()))
+        for word, count in word_to_count.items():
+            print(f"{word:{max_length}} : {count}")
+    except ValueError:
+        print("Invalid input")
+        main()
+
+
+main()
