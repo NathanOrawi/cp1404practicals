2bf448a4035d3b33eb996e6456ab08c67412e3e5
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2023-11-05 23:53:59 +1000

    Add __str__

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,17 +2,21 @@
 
 
 class Project:
-    def __init__(self):
-        pass
+    def __init__(self, name="", start_date="", priority=1, cost_estimate=0, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
 
     def __str__(self):
-        pass
+        return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage} %"
 
     def __repr__(self):
-        pass
+        return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage} %"
 
-    def __lt__(self, other):
-        pass
+    def __eq__(self, other):
+        return self.name == other.name
 
 
 def run_tests():
